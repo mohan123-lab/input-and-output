315cbcb487df84689fe847308d89e013fa1ad0e0
mohan123-lab <mohanreddygs2004@gmail.com>
2024-09-29 21:55:47 -0700

    Create mohan.py
    good

---
+++ mohan.py
@@ -0,0 +1,33 @@
+1)MS Dhoni aged X years, is a Cancerian, born with very strong Mars in his birth chart. Notably, Mars is the ruling planet for sports. Write a program to get the age of Dhoni as an integer and display the same.
+Input Format:
+Input is an integer that corresponds to the age of Dhoni.
+Output Format: 
+Display the age.
+Refer sample Input and Output for formatting specifications.
+[All the text in bold corresponds to the Input]
+Sample Input and Output:
+Enter the age:
+35
+
+
+Age of Dhoni is 35
+Ans:
+a=int(input())
+print("Age of Dhoni is",a)
+
+2)After Dhoni made it to the pinnacle of Success as Indian Captain, Mr. Banerjee was once invited by the Media to recall his association with Dhoni. Mr. Banerjee quoted one important moment that complemented the traits of Dhoni. It was when he approached Dhoni and asked him if he would play cricket for the school team saying "Will you be a wicketkeeper?" and was taken aback by Dhoni's confident reply "I will if I get a chance". Write a program to display the answers of Dhoni that impressed his master.
+Input and Output Format:
+Refer Sample input and output for formatting specifications.
+All text in bold corresponds to input and the rest corresponds to output.
+Sample Input and Output:
+Banerjee's Question:
+Will you be a wicketkeeper?
+Dhoni's Reply:
+I will if i get a chance.
+For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply was "I will if I get a chance."
+
+
+Ans:
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+
